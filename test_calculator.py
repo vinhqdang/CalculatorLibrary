262bdae253d9a1c3457df3790b1e730e1333d35d
Vinh Dang <vinhdang@192.168.1.54>
2024-11-19 16:36:45 +0700

    add test

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
+    def test_power(self):
+        assert 8 == calculator.power(2,3)
