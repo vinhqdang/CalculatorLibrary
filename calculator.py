177cf8d1baef98048f456c818e462e52fd59c186
Vinh Dang <vinhdang@AP-0323-1004.local>
2024-11-20 11:19:58 +0700

    add power function

--- calculator.py
+++ calculator.py
@@ -10,3 +10,6 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+def power (first_term, second_term):
+    return first_term ** second_term
